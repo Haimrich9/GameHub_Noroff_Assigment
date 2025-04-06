cfda62b2cebf4fac894288ccd184dcb318f6fe94
Haimrich9 <97666053+haimirich@users.noreply.github.com>
2025-04-06 23:48:58 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-/* -- Hamburger Menu functionality --
-- Toggles the class "hide" on the star and cart buttons */
-
 const createFooter = () => {
 	let footer = document.querySelector("footer");
 
@@ -9,11 +6,7 @@ const createFooter = () => {
 			<p>2024 FjellroDesign</p>
      `;
 };
-createHeader();
+
 createFooter();
 
-/* ---------
-  The code below is experimental and commented out since it’s unfinished.
-  Keeping it for future use.
-  --------- */
 let apiKey = "8ac3c186-0fba-48d5-bc32-fd2f82d128e7";
